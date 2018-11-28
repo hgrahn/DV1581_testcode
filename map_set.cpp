d07073fbe03eaa28251b2b45f33ea740cb476e02
Håkan Grahn <Hakan.Grahn@bth.se>
2018-11-28 23:43:42 +0100

    added map_set example

---
+++ map_set.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+
+int main()
+{
+    std::vector<std::string> words{"the", "quick", "red", "fox", "jumps",
+        "over", "the", "slow", "red", "turtle"};
+    std::map<std::string, size_t> word_count; // empty map from string to size_t
+
+    std::set<std::string> exclude = {"The", "But", "And", "Or", "An", "A",
+                           "the", "but", "and", "or", "an", "a"};
+
+    // count the number of times each word occurs in the input
+    for (auto w = words.cbegin(); w != words.cend(); w++)
+        ++word_count[*w]; // fetch and increment the counter for word
+
+    for (const auto &w : word_count) // for each element in the map
+        // print the results
+        std::cout << w.first << " occurs " << w.second
+            << ((w.second > 1) ? " times" : " time") << std::endl;
+
+    std::cout << std::endl;
+    word_count.clear();
+
+    // count the number of times each word occurs in the input,
+    // excluding some words
+    for (auto w = words.cbegin(); w != words.cend(); w++)
+        if (exclude.find(*w) == exclude.end())
+            ++word_count[*w]; // fetch and increment the counter for word
+
+    for (const auto &w : word_count) // for each element in the map
+        // print the results
+        std::cout << w.first << " occurs " << w.second
+            << ((w.second > 1) ? " times" : " time") << std::endl;
+}
