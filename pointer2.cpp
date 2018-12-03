d9d5ec49bc7ea1aef5d505e572ed3ef8f2d014e7
hgrahn <Hakan.Grahn@bth.se>
2018-12-03 16:50:00 +0100

    added exemple pointer2

---
+++ pointer2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <list>
+
+int main()
+{
+    std::shared_ptr<std::string> p1;    // shared_ptr that can point at a string
+    std::shared_ptr<std::list<int>> p2; // shared_ptr that can point at a list of ints
+
+    // ex 1:
+    p1 = std::make_shared<std::string>("Hello");
+    if (!p1)
+        std::cout << "p1 is null" << std::endl;
+    else {
+        std::cout << "p1 is " << *p1 << std::endl;
+        *p1 = "Goodbye";
+    }
+    if (p1)
+        std::cout << "p1 is " << *p1 << std::endl;
+}
