4d9edd8e1078c860b8af2efeda2eb3be194ae2dd
hgrahn <Hakan.Grahn@bth.se>
2018-12-05 14:37:33 +0100

    copy constructor examples

--- copy2.cpp
+++ copy2.cpp
@@ -19,12 +19,19 @@ public:
         this->s = s;
     }
 
+    /*
     TestClass(const TestClass& tc_in)
     {
         std::cout << "TestClass: copy constructor" << std::endl;
         s = tc_in.s;
     }
-
+    */
+    TestClass& operator=(const TestClass &tc_in)
+    {
+        std::cout << "TestClass: copy-assigment constructor" << std::endl;
+        s = tc_in.s;
+        return *this;
+    }
     void print()
     {
         std::cout << this->s << std::endl;
@@ -36,21 +43,14 @@ public:
     }
 };
 
-// show both call-by-value and call-by-reference
-void fcn(TestClass tc_in)
-{
-    std::cout << "Enter fcn " << std::endl;
-    std::string s("fcn");
-    TestClass tc(s);
-    std::cout << "Exit fcn " << std::endl;
-}
-
-
 int main()
 {
     std::string s("Input from main()");
     TestClass tc1(s);
-    fcn(tc1);
-    std::cout << std::endl;
-    std::cout << "Back in main()..." << std::endl;
+    std::string s2("TestClass 2");
+    TestClass tc2(s2);
+    TestClass tc3;
+    tc3 = tc2;
+    tc3.print();
+    std::cout << "End of main..." << std::endl;
 }
