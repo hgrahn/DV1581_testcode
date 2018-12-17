16340bb7fc4b8cb8243aec50299d0a0feb6045dc
hgrahn <Hakan.Grahn@bth.se>
2018-12-17 08:33:40 +0100

    random4 example update

--- random4.cpp
+++ random4.cpp
@@ -5,11 +5,14 @@
 
 int main()
 {
+    const unsigned int nvals = 500;
+    const unsigned mean = 10;
+    const double stddev = mean/3.0;
     std::default_random_engine e; // generates random integers
-    std::normal_distribution<> n(4,1.5); // mean 4, standard deviation 1.5
-    std::vector<unsigned> vals(9); // nine elements each 0
+    std::normal_distribution<> n(mean, stddev);
+    std::vector<unsigned> vals(mean*2);
 
-    for (size_t i = 0; i != 300; ++i) {
+    for (size_t i = 0; i != nvals; ++i) {
         unsigned v = lround(n(e)); // round to the nearest integer
         if (v < vals.size()) // if this result is in range
             ++vals[v]; // count how often each number appears
