2800659a9c0404fdb3cbcae00b05f23b6ec8e660
hgrahn <Hakan.Grahn@bth.se>
2019-02-05 10:08:59 +0100

    added fact1 example value vs ref call

---
+++ fact1_ref_value_call.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int fact(int val) // called by value
+{
+    int ret = 1; // local variable to hold the result as we calculate it
+    while (val > 1)
+        ret *= val--; // decrements val
+    return ret; // return the result
+}
+
+int main()
+{
+    int i = 5, factorial = 0;
+    factorial = fact(i);
+    std::cout << i << ", " << factorial << std::endl;
+}
