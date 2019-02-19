21c0cbad7d6d4f75022976b9d7334485618b15d4
hgrahn <Hakan.Grahn@bth.se>
2019-02-19 09:04:35 +0100

    dyn mem examples update

--- pointer1.cpp
+++ pointer1.cpp
@@ -6,6 +6,7 @@ struct TwoInts
 {
     int a, b;
     TwoInts() {a = 2; b = 4;}
+    TwoInts(int init_a, int init_b) : a(init_a), b(init_b) {};
     void print(const std::string s) {
         std::cout << s << ": a = " << a << ", b = " << b << std::endl;
     }
@@ -18,18 +19,22 @@ int main()
 
     ti.print("ti");
 
-    //ptr_ti->print("ptr_ti");
+    ptr_ti->print("ptr_ti");
 
-    ptr_ti = new TwoInts();
+    ptr_ti = new TwoInts(5, 7);
     ptr_ti->print("ptr_ti2");
 
-/*
     ti.a = 23;
     ti.print("ti");
 
+    ptr_ti->b = 31;
+    ptr_ti->print("ptr_ti2");
+    
+    return 0;
+
     ptr_ti = &ti;
     ptr_ti->print("ptr_ti2");
-*/
+
     delete ptr_ti;
     ptr_ti = nullptr;
     // ptr_ti: now a dangling pointer => danger!
