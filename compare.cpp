920d00839a2ff8a56b12525de24a13ab62ef619f
hgrahn <Hakan.Grahn@bth.se>
2019-03-06 11:08:32 +0100

    update of template examples 2019vt

--- compare.cpp
+++ compare.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 template <typename T>
@@ -13,6 +14,13 @@ int main()
 {
     // instantiates int compare(const int&, const int&)
     std::cout << compare(1, 0) << std::endl;       // T is int
+
+    // instantiates int compare(const std::string&, const std::string&)
+    std::cout << compare(std::string("Hej"), std::string("Hopp")) << std::endl;  // T is std::string
+    std::cout << compare(std::string("hej"), std::string("Hej")) << std::endl;
+    std::cout << compare(std::string("Hej"), std::string("Hej")) << std::endl;
+
+    //std::cout << compare(std::string("hej"), 4) << std::endl;
 
     // instantiates int compare(const vector<int>&, const vector<int>&)
     std::vector<int> vec1{1, 2, 3}, vec2{4, 5, 6};
--- testtemplate.cpp
+++ testtemplate.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
 template <typename T> class TestTemplate {
 private:
